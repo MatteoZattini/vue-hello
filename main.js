bad314e63dc6c1fc8e7a319cbb6d4aab5868bdd2
MatteoZattini <matteozattini@hotmail.com>
2024-05-10 18:21:48 +0200

    added click event to img

--- main.js
+++ main.js
@@ -5,13 +5,26 @@ createApp({
     return {
       message: 'A belli',
       text: 'ciao',
-      immagine: './img/01.webp'
+      immagine: './img/01.webp',
+      immagine2: './img/02.webp',
+      
     }
   },
 
   methods: {
     cambia() {
         console.log("spooderman")
+        let x = document.getElementById("img1")
+        let y = document.getElementById("img2")
+
+        if(x.classList.contains("d-none")){
+          x.classList.remove("d-none")
+          y.classList.add("d-none")
+        } else {
+          x.classList.add("d-none")
+          y.classList.remove("d-none")
+        }
+
     }
   }
 }).mount('#app')
